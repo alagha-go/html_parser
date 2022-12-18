7b2383eb2148c867234a7627d4b3bf3a5200b642
alagha-go <abdihakimosman1480@gmail.com>
2022-12-18 06:00:53 +0300

    create necessary types

---
+++ src/parser/mod.rs
@@ -0,0 +1,6 @@
+use pest::{iterators::Pairs, Parser};
+use std::collections::{HashMap, HashSet};
+use std::collections::VecDeque;
+use pest_derive::Parser;
+
+include!("structs.rs");
---
+++ src/parser/structs.rs
@@ -0,0 +1,33 @@
+pub type Attributes = HashMap<String, String>;
+
+#[derive(Debug, PartialEq, Eq, Clone, Default)]
+pub struct Dom {
+    pub documents: Vec<Document>
+}
+
+#[derive(Debug, PartialEq, Eq, Clone)]
+pub enum Document {
+    Element(Node),
+    Text(String),
+    Comment(String)
+}
+
+#[derive(Debug, PartialEq, Eq, Clone, Default)]
+pub struct Node {
+    pub name: String,
+    pub id: Option<String>,
+    pub classes: HashSet<String>,
+    pub attributes: Attributes,
+    pub children: Vec<Document>
+}
+
+
+pub enum Action<T> {
+    Call(T),
+    Handle(T)
+}
+
+
+#[derive(Parser)]
+#[grammar = "./src/parser/rules.pest"]
+pub struct Grammar;
